a1cb7f66f744d73106ec563aa8dfb4653597d169
cucugbl <gabriel@MacBook-Pro-de-Gabriel.local>
2020-05-28 17:35:11 -0500

    se quito repeticiones de palabras en temas y autores

--- Lematizacion/Resumen.py
+++ Lematizacion/Resumen.py
@@ -6,7 +6,7 @@ from nltk import SnowballStemmer
 class Resumen:
     nlp = spacy.load("es_core_news_sm")
     spanishstemmer=SnowballStemmer("spanish")
-    palabrasClave = []
+    palabrasClave = ['gobi', 'gobern', 'secretari', 'centr', 'univers', 'escuel', 'institut', 'ministeri','gobiern', 'alcald', 'republ', 'subsecretari', 'viceministeri', 'director', 'gerent', 'dueñ', 'propietari']
     doc = ''
     listaPalabrasLematizadas=[]
     entMiscEvNacProdObr=[]
@@ -18,14 +18,30 @@ class Resumen:
     palabrasEtiquetadasRepetidas = []
     autoresStemming = []
 
+    #variables auxiliares
+    posicionesParaBorrarEnLemma=[]
 
+    def __init__(self, texto):
+        if(texto!=""):
+            self.doc = ''
+            self.listaPalabrasLematizadas=[]
+            self.entMiscEvNacProdObr=[]
+            self.etiquetadas=[]
+            self.frasesDosPalabras = []
+            self.frasesTresPalabras = []
+            self.frasesTresPalabrasNoRepetidas = []
+            self.frasesDosPalabrasNoRepetidas = []
+            self.palabrasEtiquetadasRepetidas = []
+            self.autoresStemming = []
+            self.posicionesParaBorrarEnLemma=[]
+            self.doc = self.nlp(texto)
+            self.lematizarPalabras()
 
-    def __init__(self, palabrasClave, texto):
-        self.palabrasClave = palabrasClave
-        self.doc = self.nlp(texto)
-        self.lematizarPalabras()
-        self.etiquetarPalabras()
-
+    def procesarPalabrasClave(self, palabrasClaveRecibidas):
+        if(palabrasClaveRecibidas!=""):
+            self.palabrasClave = []
+            self.palabrasClave = [self.spanishstemmer.stem(i) for i in palabrasClaveRecibidas]
+                
     def etiquetarPalabras(self):
         for e in self.doc.ents:
             if e .label_=='MISC':
@@ -47,10 +63,25 @@ class Resumen:
 
     def raizPalabra(self):
         raicesPalabras = [self.spanishstemmer.stem(token) for token in self.listaPalabrasLematizadas]
-        for i, rp in enumerate(raicesPalabras):
+        i=0
+        while i < len(raicesPalabras):
             for pc in self.palabrasClave:
-                if rp == pc:
-                    self.autoresStemming.append(self.listaPalabrasLematizadas[i]+" "+self.listaPalabrasLematizadas[i+1]+" "+self.listaPalabrasLematizadas[i+2])
+                if raicesPalabras[i] == pc:
+                    if i+3 == len(raicesPalabras):
+                        self.autoresStemming.append(self.listaPalabrasLematizadas[i]+" "+self.listaPalabrasLematizadas[i+1])
+                    if i+2 >= len(raicesPalabras):
+                        break
+                    else:    
+                        self.autoresStemming.append(self.listaPalabrasLematizadas[i]+" "+self.listaPalabrasLematizadas[i+1]+" "+self.listaPalabrasLematizadas[i+2])
+                    self.posicionesParaBorrarEnLemma.append(self.listaPalabrasLematizadas[i])
+                    i+=1
+                    break
+            i+=1
+        self.quitarPalabrasUsadasEnRaiz()
+
+    def quitarPalabrasUsadasEnRaiz(self):
+        for i in self.posicionesParaBorrarEnLemma:
+            self.listaPalabrasLematizadas.remove(i)
         self.juegosPalabras()
 
     def juegosPalabras(self):
@@ -84,7 +115,4 @@ class Resumen:
         for i in range(0, len(frasesFrecuenteRepetidas), 2):
             self.frasesDosPalabrasNoRepetidas.append(frasesFrecuenteRepetidas[i][0])
 
-noticia= Resumen(['gobi', 'gobern', 'secretari', 'centr', 'univers', 'escuel', 'institut', 'ministeri','gobiern', 'alcald', 'republ', 'subsecretari', 'viceministeri', 'director', 'gerent', 'dueñ', 'propietari'],"""Autoridades sanitarias de México reportaron este viernes 28 de febrero del 2020 el tercer caso del nuevo coronavirus, al confirmar un nuevo infectado en la capital, que también dio positivo a las pruebas de laboratorio luego de viajar a Italia. “Hasta este momento tenemos tres casos confirmados del nuevo coronavirus. Probablemente los tres se contagiaron de una misma fuente. Los tres son casos importados”, dijo José Luis Alomía, director general de epidemiología de México, en una conferencia de prensa. Horas antes, el Gobierno de Sinaloa, estado del noroeste de México, confirmó el segundo caso de coronavirus en el país, tras realizarle una segunda prueba a un paciente que estuvo también en Italia con el hombre de Ciudad de México que fue el primer contagio conocido en el territorio. \"En Culiacán (capital del estado) hay un caso confirmado de Covid-19, se han seguido todos los protocolos de contención y no debería de pasar a mayores. Estemos pendientes\", escribió en Twitter el gobernador Quirino Ordaz tras una reunión con la Secretaría de Salud estatal. El paciente de 41 años, originario del céntrico estado de Hidalgo, voló desde Ciudad de México a Culiacán, donde estuvo aislado en un hotel en el que se realizaban las dos pruebas confirmatorias. En la misma red social, el secretario de Salud del estado, Efrén Encinas, afirmó tener \"todo contenido y aislado\" en coordinación con la Secretaría de Salud federal y estar preparado para \"atender cualquier caso\". \"A cuidarnos. Seguimos con un caso de Covid-19 confirmado en Sinaloa, pero bien atendido y sin peligro de más contagios\", publicó el funcionario estatal. Horas antes, cuando el caso apenas era sospechoso, el también médico había ofrecido una rueda de prensa en la que explicó que el paciente padecía una sintomatología leve, con un poco de fiebre y dolor de cabeza. Precisó que este caso está relacionado con el caso positivo que se dio en Ciudad de México porque ambas personas estuvieron días antes en un congreso en el norte de Italia, al asegurar que esta persona estaba en Culiacán para dar una conferencia relacionada con su trabajo. Durante la conferencia matutina del presidente de México, Andrés Manuel López Obrador, las autoridades federales confirmaron este viernes el primer caso de coronavirus del país, el segundo detectado en América Latina después del reportado en Brasil. Hugo López-Gatell, subsecretario de Prevención y Promoción de la Salud, señaló que el positivo pertenece a un hombre de 35 años que está hospitalizado en el Instituto Nacional de Enfermedades Respiratorias (INER). Los dos primeros casos están relacionados con un mexicano residente en Bérgamo, en el norte de Italia, lugar donde se produjeron los contagios en la tercera semana de febrero. Aunque los funcionarios de salud han pedido evitar el pánico, recomiendan a la población lavarse las manos de manera constante, tapar su boca al toser o estornudar, evitar tocarse la cara y no saludar de abrazo o de beso a las personas desconocidas. También han alertado por noticias falsas que se difunden por redes sociales, por lo que exhortan a solo hacer caso a fuentes oficiales.
-""")
-print(noticia.frasesDosPalabrasNoRepetidas)
 
--- Lematizacion/recorrerJSON.py
+++ Lematizacion/recorrerJSON.py
@@ -1,8 +1,37 @@
 import json
-
+from Resumen import Resumen
 
 with open('Git/noticias/noticias.json') as file:
     data = json.load(file)
-    for noticia in data['noticias']:
-        print('Diario:', noticia['Diario'])
-        
+    for i,noticia in enumerate(data['noticias']):
+        print(i,"@@@@@@")
+        print(noticia['Titulo'])
+        resumen=Resumen(noticia['Texto'])
+        print('Autores:', resumen.autoresStemming)
+        print('Frases dos palabras:', resumen.frasesTresPalabrasNoRepetidas)
+      
+
+
+
+
+"""
+diccionario de archivo texto 
+enriquesiendo diccionario segun las noticias
+mandar al correo
+
+
+si me sale como auto, que no me salga como tema
+corregir organizacion panarica y salud publica que no salga en tema 
+
+
+
+redactar dos cosasplaneap
+def
+
+1teprica de lo que estamos hciedno 
+1.1descripcion de librerias y tecnicas que stamos usando 
+1.2con referencias
+1.3que es normalizar steemin raices, etc
+s
+doac bdd
+si se necesita el numero de veces que se repite"""
