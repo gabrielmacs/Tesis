800a2d612677aaf487855c8f82e4db7967632798
paulsteban <33968917+paulsteban@users.noreply.github.com>
2020-10-14 12:49:01 -0500

    arregloarchivodañado
    se arreglo q el archivo se rellenaba con los residuos anteriores

--- noticias_articulos/nyt/apinewyorktimes.py
+++ noticias_articulos/nyt/apinewyorktimes.py
@@ -5,7 +5,7 @@
 import time
 import requests
 import re
-arreglolinksnoticias=[]
+#arreglolinksnoticias=[]
 def execute(a,arreglolinksnoticias,query):
   print(query)
   print(re.sub(" ","%20",query))
@@ -22,7 +22,7 @@ def execute(a,arreglolinksnoticias,query):
   #print(request.json())
   
   if(request.status_code==200):
-    time.sleep(6)  
+    #time.sleep(6)  
     respuestajson =request.json()
     noticiasjson=respuestajson['response']
     for documento in noticiasjson['docs']:
