5b9bc993fd39d6efb63ebc022a0c669fee3bb642
paulsteban <33968917+paulsteban@users.noreply.github.com>
2020-10-16 17:47:08 -0500

    paginacion dinamica doaj

--- noticias_articulos/doaj/apidoaj.py
+++ noticias_articulos/doaj/apidoaj.py
@@ -12,13 +12,13 @@ data['datadoaj']=[]
 #cantidad=10
 #query="Energia"
 
-def execute(a,query,idioma):
+def execute(a,query,idioma,orden):
   print(query)
   print(re.sub(" ","%20",query))
   queryf=re.sub(" ","%20",query)
   paginabusqueda=str(a)  
   print("********************Numero de pagina"+str(a)) 
-  requestUrl = "https://doaj.org/api/v1/search/articles/(title%3A%22"+queryf+"%22)%20OR%20(bibjson.abstract%3A%22"+queryf+"%22)?page="+paginabusqueda+"&pageSize=100"
+  requestUrl = "https://doaj.org/api/v1/search/articles/(title%3A%22"+queryf+"%22)%20OR%20(bibjson.abstract%3A%22"+queryf+"%22)%20AND%20(bibjson.journal.language%3A%22"+idioma+"%22)?page="+paginabusqueda+"&pageSize=100&sort=year%3A"+orden
   print(requestUrl)
   requestHeaders = {
     "Accept": "application/json"
@@ -31,31 +31,73 @@ def execute(a,query,idioma):
   if(request.status_code==200):
     respuestajson =request.json()
     print("total archivos "+str(respuestajson['total']))
-    if(respuestajson['total']>0):
+    if(respuestajson['total']!=0):
       noticiasjson=respuestajson['results']
+      print("sdads")
       #print(noticiasjson[0])
-      
       for documento in noticiasjson:
           articulo = documento['bibjson']
-          metaarticulo = articulo['journal']
+          #metaarticulo = articulo['journal']
          
           #print(documento['bibjson'])
-          if('abstract' in articulo  ):
+          if('abstract' in articulo):
             #and 'EN' in articulo['language']
-            if( idioma in metaarticulo['language']):
-              #i = i + 1
-              #print(metaarticulo['language'])
-              data['datadoaj'].append({'text': articulo['abstract']})
-              
+            #if( idioma in metaarticulo['language']):
+            #i = i + 1
+            #print(metaarticulo['language'])
+            data['datadoaj'].append({'text': articulo['abstract']})
+            print(articulo['abstract'])  
             
             #arregloarticulos.pop()  
-          
+          else:
+            print(articulo['abstract'])
     else:
       print("No se encontro resultados")  
   
   #print(arregloarticulos[1]['abstract'])
- 
+def cantidadpagina(a,query,idioma):
+  queryf=re.sub(" ","%20",query)
+  paginabusqueda=str(a)  
+  requestUrl = "https://doaj.org/api/v1/search/articles/(title%3A%22"+queryf+"%22)%20OR%20(bibjson.abstract%3A%22"+queryf+"%22)%20AND%20(bibjson.journal.language%3A%22"+idioma+"%22)?page="+paginabusqueda+"&pageSize=100"
+  #"http://api.springernature.com/meta/v2/json?q=(title%3A%22"+queryf+"%22%20OR%20keyword%3A%22"+queryf+"%22)&p=100&s="+paginabusqueda+"&api_key=3baefd29b9ce65a833af4889a0cacbed"
+  #print(requestUrl)
+  requestHeaders = {
+    "Accept": "application/json"
+  }
 
+  request = requests.get(requestUrl, headers=requestHeaders)
+  #print(request.json())
+  
+  if(request.status_code==200):
+    respuestajson =request.json()
+    print("total archivos "+str(respuestajson['total']))
+    
+    if(((respuestajson['total']//100)+1)>10):
+      numarticulo = 11
+    else:
+      numarticulo = (respuestajson['total']//100)+2  
+      
+  return (numarticulo)
+
+def cantidadarticulos2(a,query,idioma):
+  queryf=re.sub(" ","%20",query)
+  paginabusqueda=str(a)  
+  requestUrl = "https://doaj.org/api/v1/search/articles/(title%3A%22"+queryf+"%22)%20OR%20(bibjson.abstract%3A%22"+queryf+"%22)%20AND%20(bibjson.journal.language%3A%22"+idioma+"%22)?page="+paginabusqueda+"&pageSize=100"
+  #"http://api.springernature.com/meta/v2/json?q=(title%3A%22"+queryf+"%22%20OR%20keyword%3A%22"+queryf+"%22)&p=100&s="+paginabusqueda+"&api_key=3baefd29b9ce65a833af4889a0cacbed"
+  #print(requestUrl)
+  requestHeaders = {
+    "Accept": "application/json"
+  }
+
+  request = requests.get(requestUrl, headers=requestHeaders)
+  #print(request.json())
+  
+  if(request.status_code==200):
+    respuestajson =request.json()
+    print("total archivos "+str(respuestajson['total']))       
+    numarticulo = (respuestajson['total']//100)+2  
+      
+  return (numarticulo)
 '''
 #execute(1,arregloarticulos,"Shadow")
 with open('datadoaj.json','w', encoding='utf-8') as file:
--- noticias_articulos/doaj/funcionesdoaj.py
+++ noticias_articulos/doaj/funcionesdoaj.py
@@ -2,9 +2,10 @@ import apidoaj as apidoaj
 import json
 import codecs
 
-cantidad=11
-query="Cama"
-idioma= "ES"
+#cantidad=1
+query="Hydrostatic"
+idioma= "EN"
+
 '''
 with open('datadoaj.json','w', encoding='utf-8') as file:
     for n in range(cantidad):
@@ -16,11 +17,19 @@ with open('datadoaj.json','w', encoding='utf-8') as file:
     print(len(arregloarticulos))
 '''
  #execute(1,arregloarticulos,"Shadow")
-for n in range(1,cantidad): 
-       
+#print(apidoaj.cantidadarticulos(1,query)) 
+'''for n in range(1,apidoaj.cantidadarticulos(1,query,idioma)):       
     apidoaj.execute(n,query,idioma)  
-
-with open('datadoaj.json','w', encoding='utf-8') as file:
+'''
+if(apidoaj.cantidadarticulos2(1,query,idioma)>20):
+    for n in range(1,apidoaj.cantidadpagina(1,query,idioma)):       
+        apidoaj.execute(n,query,idioma,"asc")  
+    for m in range(1,apidoaj.cantidadpagina(1,query,idioma)):       
+        apidoaj.execute(m,query,idioma,"desc")
+else:
+    for m in range(1,apidoaj.cantidadpagina(1,query,idioma)):       
+        apidoaj.execute(m,query,idioma,"desc")
+with open('datadoajx.json','w', encoding='utf-8') as file:
     
     json.dump(apidoaj.data,file,indent=4,ensure_ascii=False)  
   #print(nnyt.get_news_nyt(url))    
